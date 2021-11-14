53cfecf9f53a679148f122cfbc8a9c185100b14b
dekelrachel@gmail.com <dekelrachel@gmail.com>
2021-11-14 22:51:15 +0200

    step 5

--- js/myfunctions.js
+++ js/myfunctions.js
@@ -5,31 +5,69 @@ class Task {
         this.priority = priority;
     }
 }
+
 const submit = document.getElementById("add");
 let taskList = [];
 submit.addEventListener('click', function () {
     const titleName = document.getElementById("textbox").value;
     const titleDesc = document.getElementById("Description").value;
-/*    console.log(titleName);
-    console.log(titleDesc);*/
+    const isPriority = document.getElementById("checkme").checked;
+    console.log(isPriority);
     //check
-    let newTask = new Task(titleName,titleDesc,true);
+    let newTask = new Task(titleName, titleDesc, isPriority);
     taskList.push(newTask);
-    let cardTitle = document.createElement("h5");
-    let cardDescription  = document.createElement("h6");
-    //delete button
-    cardTitle.appendChild(document.createTextNode(titleName));
-    cardDescription.appendChild(document.createTextNode(titleDesc));
-    let card = document.createElement("div");
-    card.appendChild(cardTitle);
-    card.appendChild(cardDescription);
-    let placeForTasks = document.getElementById("taskstoddo");
-    placeForTasks.appendChild(card);
-    console.log(taskList);
+    printList();
+    let deleteButtons = document.getElementsByClassName("deleteButton");
+    for (let but of deleteButtons)
+        but.addEventListener('click', function (event) {
+            const index = taskList.findIndex(ele => ele.title == this.parentElement.firstElementChild.innerHTML)
+            //console.log(index)
+            //console.log(event.target.parentElement.firstElementChild.innerHTML);
+            this.parentElement.remove();
+            taskList.splice(index,1);
+            //console.log(listOfTasksJS)
+        } );
+
+})
+function printList()
+    {
+        let htmlList = document.getElementById('taskstoddo');
+        htmlList.innerHTML = "<div id = 'taskstoddo'>";
+        for(let task of taskList)
+            {
+                htmlList.innerHTML+= printTask(task);
+            }
+        htmlList.innerHTML +="</div>";
+        console.log(htmlList);
+    }
+function printTask(t) {
+    let ret = "<h3>" + t.title + "</h3>";
+    ret += "<h4> " +t.description + "</h4>";
+    ret +=  "<button class = 'deleteButton'>delete</button> ";
+    return ret;
+}
+
     //clear textbox
     //print task
 
-})
+
+function deleteTask(event) {
+
+    event.target.parentElement.remove();
+}
+
+/*function errorMessage(msg) {
+    document.getElementById("errormsg").innerHTML = msg;
+    document.getElementById("errormsg").style.display = "block";
+}
+
+/!*
+reset the error message (erase, hide)
+ *!/
+function resetErrorMessage() {
+    document.getElementById("errormsg").innerHTML = "";
+    document.getElementById("errormsg").style.display = "none";
+}*/
 
 // let addtTask = (task) =>
 // {
